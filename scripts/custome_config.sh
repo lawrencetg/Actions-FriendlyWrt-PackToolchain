3b726893f8ef284036fb705f1a04e55c4daf8977
Lawrence-Tang <tangzongsheng@gmail.com>
2022-11-27 15:41:00 +0800

    pack toolchain

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
-sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+# sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
+# sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+
+(cd configs/rockchip/ && {
+    mv 01-nanopi ../
+    rm *
+    mv ../01-nanopi .
+})
